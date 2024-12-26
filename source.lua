0fff7fcf961239b84871929afe9a7f602d67f1d3
DELETIONSs <officialkadencreations@gmail.com>
2024-12-26 09:10:58 -0500

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,6 @@
+local UserInputService = game:GetService("UserInputService")
+local TweenService = game:GetService("TweenService")
+local RunService = game:GetService("RunService")
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local Mouse = LocalPlayer:GetMouse()
+local HttpService = game:GetService("HttpService")
